62190703a56ba6ca263655f6e26570f35546dfcb
abd-zeyad <abd.d@live.com>
2022-01-31 01:15:11 +0200

    add auth using google

--- screens/Auth/SignUpScreen.tsx
+++ screens/Auth/SignUpScreen.tsx
@@ -1,11 +1,31 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import * as Google from 'expo-google-app-auth';
 
 const SignUpScreen = () => {
     const navigation = useNavigation()
+    const [token, setToken] = useState<string | null>();
     const goToTabOne = () => {
         navigation.navigate('Main', { screen: 'TabOne' })
+    }
+    async function signInWithGoogleAsync() {
+        try {
+            const result = await Google.logInAsync({
+                androidClientId: "590049508497-cleb0uumqau94ngsfejcpihiab1um5r9.apps.googleusercontent.com",
+                iosClientId: "590049508497-j0gqvuh9903t1tgf2l90sc2jbkho0frl.apps.googleusercontent.com",
+                scopes: ['profile', 'email'],
+            });
+
+            if (result.type === 'success') {
+                setToken(result.accessToken)
+                return result.accessToken;
+            } else {
+                return { cancelled: true };
+            }
+        } catch (e) {
+            return { error: true };
+        }
     }
     return (
         <View>
@@ -16,8 +36,12 @@ const SignUpScreen = () => {
             >
                 <Text>go to tab one screen</Text>
             </TouchableOpacity>
+            <Button onPress={signInWithGoogleAsync} title='sign in' />
+            <Text>{token}</Text>
         </View>
     );
 };
 
 export default SignUpScreen;
+
+
