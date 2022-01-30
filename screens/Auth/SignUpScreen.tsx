dcf0fd1c91193cd70ed2769e07922a4263d2d444
abd-zeyad <abd.d@live.com>
2022-01-31 01:53:51 +0200

    finish google sign in

--- screens/Auth/SignUpScreen.tsx
+++ screens/Auth/SignUpScreen.tsx
@@ -1,48 +1,54 @@
-import { View, Text, TouchableOpacity, Button } from 'react-native';
-import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import * as Google from 'expo-google-app-auth';
-
-const SignUpScreen = () => {
-    const navigation = useNavigation()
-    const [token, setToken] = useState<string | null>();
-    const goToTabOne = () => {
-        navigation.navigate('Main', { screen: 'TabOne' })
-    }
-    async function signInWithGoogleAsync() {
-        try {
-            const result = await Google.logInAsync({
-                androidClientId: "590049508497-cleb0uumqau94ngsfejcpihiab1um5r9.apps.googleusercontent.com",
-                iosClientId: "590049508497-j0gqvuh9903t1tgf2l90sc2jbkho0frl.apps.googleusercontent.com",
-                scopes: ['profile', 'email'],
-            });
-            console.log(result);
-
-            if (result.type === 'success') {
-                setToken(result.accessToken)
-                return result.accessToken;
-            } else {
-                return { cancelled: true };
-            }
-        } catch (e) {
-            return { error: true };
-        }
-    }
-    return (
-        <View>
-            <Text>SignUpScreen</Text>
-            <TouchableOpacity
-                onPress={goToTabOne}
-                style={{ height: 100, backgroundColor: 'red' }}
-            >
-                <Text>go to tab one screen</Text>
-            </TouchableOpacity>
-            <Button onPress={signInWithGoogleAsync} title='sign in' />
-            <Text>{token}</Text>
-        </View>
-    );
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { ResponseType } from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import { Button, View } from 'react-native';
+const firebaseConfig = {
+    apiKey: "AIzaSyD4l3PgAkbTTFGCc1I7Ea5KN-FqdL8OMtY",
+    authDomain: "uni-project-6f3be.firebaseapp.com",
+    projectId: "uni-project-6f3be",
+    storageBucket: "uni-project-6f3be.appspot.com",
+    messagingSenderId: "1035818317432",
+    appId: "1:1035818317432:web:c4688a51a12afe66885f8d"
 };
 
-export default SignUpScreen;
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
+WebBrowser.maybeCompleteAuthSession();
+
+export default function App() {
+
+    const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+        {
+            clientId: '1035818317432-tedj0hgegk4ao7f1tvv819ip1enql43f.apps.googleusercontent.com',
+        },
+    );
+
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const { id_token } = response.params;
+            const auth = getAuth();
+            const credential = GoogleAuthProvider.credential(id_token);
+            console.log(auth, 'authshit');
+
+            signInWithCredential(auth, credential);
+        }
+    }, [response]);
+
+    return (
+        <View>
+            <Button
+                disabled={!request}
+                title="Login"
+                onPress={() => {
+                    promptAsync();
+                }}
+            />
+        </View>
+
+    );
+}
 
